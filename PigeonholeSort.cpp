4e8073dcf2736faa1b89aea1878d228f514a3a78
2309harshit <72090538+2309harshit@users.noreply.github.com>
2021-10-04 14:00:31 +0530

    Create PigeonholeSort.cpp

---
+++ PigeonholeSort.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void pigeonhole(int arr[], int n)
+{
+  
+    int min = arr[0], max = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] < min)
+            min = arr[i];
+        if (arr[i] > max)
+            max = arr[i];
+    }
+    int range = max - min + 1; 
+  
+    vector<int> holes[range];
+
+    for (int i = 0; i < n; i++)
+        holes[arr[i]-min].push_back(arr[i]);
+ 
+    int index = 0; 
+    for (int i = 0; i < range; i++)
+    {
+       vector<int>::iterator it;
+       for (it = holes[i].begin(); it != holes[i].end(); ++it)
+            arr[index++]  = *it;
+    }
+}
+
+int main()
+{
+    int n; // size of array
+    cin>>n;  
+    int arr[n];
+     for(int i=0;i<n;i++){
+       cin>>arr[i];
+     }
+ 
+    pigeonhole(arr, n);
+ 
+    printf("Sorted arr is : ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+ 
+    return 0;
+}
