07cfd7a5bdccd56f128576d073da015477fc1bc2
pronoy <vedchak2020@gmail.com>
2021-10-04 13:31:32 +0530

    init

--- C++/quicksort.cpp
+++ C++/quicksort.cpp
@@ -1,70 +1,74 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-//Quick Sort Implementation
-
-int K = 0; int n;
-
-//arrange the elements right and left so that all left elements are small and all right elements are greater
-int arrange(int *arr, int low, int high)
+#include<bits/stdc++.h>
+void swap(int *a,int *b)
+    
 {
-    int pivot = arr[high];
-    int i = low - 1;
-    for (int j = low; j <= high - 1; j++)
+    int c;
+     c=*a;
+    *a=*b;
+    *b=c;
+    
+}
+int partition(int input[],int s,int e){
+    int count =0;
+    int x=input[s];
+    int i;
+    for(i=s;i<=e;i++)                                                       
     {
-        if (arr[j] <= pivot)
+        if(input[i]<x)
         {
-            i++;
-            swap(arr[i], arr[j]);
+            count++;
         }
     }
-    return i + 1;
+    i=0;
+    int j=e;
+    swap(&input[s+count],&input[s]);
+    while(i<(s+count)&&j>(s+count)){
+        if(input[i]<x){
+            i++;
+        }
+        else if(input[j]>=x){
+            j--;
+        }
+        else{
+            swap(&input[i],&input[j]);
+        }
+    }
+    
 }
-
-//randomized partition function
-int partition(int *arr, int low, int high)
+void sort(int input[],int s,int e)
+{if(s>=e)
 {
-    srand(time(0));
-    int random = low + (rand() % (high - low));
-    swap(arr[random], arr[high]);
-    return arrange(arr, low, high);
-}
-
-//quick funtion that calls partition and sorts recursively by arranging one element
-void quicksort(int *arr, int low, int high)
-{
-    if (low >= high)
-        return;
-    int pi = partition(arr, low, high);
-    quicksort(arr, low, pi - 1);
-    quicksort(arr, pi + 1, high);
     return;
 }
-
-int main()
-{
-    //taking inputs
-    cin >> n;
-    int *arr = new int[n];
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-
-    //starting timer before sort
-    auto start = std::chrono::system_clock::now();
-    //sorting
-    quicksort(arr, 0, n - 1);
-    //stoping the timer
-    auto end = std::chrono::system_clock::now();
-    //calculating the time taken by quick sort and printing
-    chrono::duration<double> elapsed_seconds = end - start;
-    time_t end_time = chrono::system_clock::to_time_t(end);
-    cout << "Elapsed time: " << elapsed_seconds.count() << "s\n";
-
-    //printing the sorted array
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
-
-    //deleting the array
-    delete []arr;
-    return 0;
+   int c = partition(input,s,e);
+    sort(input,s,c-1);
+    sort(input,c+1,e);
 }
+void quickSort(int input[], int size) {
+ 
+ sort(input,0,size-1);
+}
+
+int main(){
+    int n;
+    cin >> n;
+  
+    int *input = new int[n];
+    
+    for(int i = 0; i < n; i++) {
+        cin >> input[i];
+    }
+    
+    quickSort(input, n);
+    for(int i = 0; i < n; i++) {
+        cout << input[i] << " ";
+    }
+    
+    delete [] input;
+
+}
+
+
