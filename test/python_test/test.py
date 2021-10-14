71e954291289f5db574d0d452a0c321d3e7fa6d4
strikeraryu <striker.aryu56@gmail.com>
2021-10-14 16:15:01 +0530

    moved python test

--- test/python_test/test.py
+++ test/python_test/test.py
@@ -1,6 +1,6 @@
 import sys
 from pathlib import *
-sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent.parent))
 
 from Python import bubbleSort, insertionSort, mergeSort, quickSort, radixSort, selection_sort, shell_sort, sleepSort
 
